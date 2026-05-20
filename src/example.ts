8f370562ceec755986d6d7642bff92d58f0660cd
JFronny <git@akbvopenfl1.hopto.org>
2026-05-20 13:22:27 +0200

    docs: add comments to snippets included in about page

--- src/example.ts
+++ src/example.ts
@@ -1,19 +1,23 @@
+// See below for the full TypeScript interface
 import type { WheelConfig } from "src/types";
 
-export const exampleConfig: WheelConfig = {
-  hash: { type: "current" },
-  options: [
-    { label: "Option 1", id: "1", weight: 1, color: "red" },
-    { label: "Option 2", id: "2", weight: 2, color: "blue" },
-    { label: "Option 3", id: "3", weight: 1, color: "green" },
-  ],
-  actions: [
-    { name: "Search by ID", template: "https://google.com/search?q={}" },
-    { name: "Search by Label", template: "https://google.com/search?q={label}" },
-  ],
-};
-
 export function getExampleUrl() {
+  // Put your options and actions here
+  const exampleConfig: WheelConfig = {
+    hash: { type: "current" },
+    options: [
+      { label: "Option 1", id: "1", weight: 1, color: "red" },
+      { label: "Option 2", id: "2", weight: 2, color: "blue" },
+      { label: "Option 3", id: "3", weight: 1, color: "green" },
+    ],
+    actions: [
+      { name: "Search by ID", template: "https://google.com/search?q={}" },
+      { name: "Search by Label", template: "https://google.com/search?q={label}" },
+    ],
+  };
+
+  // The config is encoded as json and passed as a query parameter.
+  // Make sure to encode it properly to avoid issues with special characters!
   const json = JSON.stringify(exampleConfig);
   const encoded = encodeURIComponent(json);
   return `${import.meta.env.BASE_URL}wheel?config=${encoded}`;
--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,10 @@
+/**
+ * A wheel option/sector
+ * @prop id unique identifier to be passed to the action template, defaults to the label
+ * @prop label option label for display
+ * @prop weight weight of the option, defaults to 1
+ * @prop color color of the option, defaults to a random color
+ */
 export interface WheelOption {
   id?: string;
   label: string;
@@ -5,17 +12,46 @@ export interface WheelOption {
   color?: string;
 }
 
+/**
+ * A wheel action.
+ * After a result is determined, all actions will be shown to the user as links they can click
+ * @prop name name of the action
+ * @prop template template string to be used for the action,
+ *                can contain {label} to be replaced with the option label,
+ *                and {id} to be replaced with the option id
+ */
 export interface WheelAction {
   name: string;
   template: string;
 }
 
+/**
+ * The source of the hash to be used for the wheel
+ * If not provided, the default is {@link DefaultHashSource}
+ */
 export type HashSource = "Bitcoin" | "Monero"
 export const DefaultHashSource: HashSource = "Bitcoin";
+
+/**
+ * A reference to a hash.
+ * @prop type type of hash reference, can be "historic", "current", or "next"
+ *            If "historic", the hash is provided as part of the reference.
+ *            If "current", the current hash is fetched from the blockchain.
+ *            If "next", wait until a new block is mined and use the hash from that block.
+ * @prop hash hash value, either a hex string or a base64 string
+ * @prop source source of the hash, defaults to {@link DefaultHashSource}
+ */
 export type HashRef = { type: "historic"; hash: string; source?: HashSource }
   | { type: "current"; source?: HashSource }
   | { type: "next"; source?: HashSource };
 
+/**
+ * A complete wheel configuration.
+ * This combines all the information needed to create a wheel.
+ * @prop hash hash source
+ * @prop options list of wheel options
+ * @prop actions list of wheel actions
+ */
 export interface WheelConfig {
   hash: HashRef;
   options: WheelOption[];
